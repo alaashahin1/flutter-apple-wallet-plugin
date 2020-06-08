14ed3650bcbb6a880d8ce33e3d311cce5b10a6ce
jordg2015 <jordy.g2012@gmail.com>
2020-06-07 19:55:00 -0600

    swift class updated

--- ios/Classes/SwiftWalletPlugin.swift
+++ ios/Classes/SwiftWalletPlugin.swift
@@ -1,4 +1,5 @@
 import Flutter
+import PassKit
 import UIKit
 
 public class SwiftWalletPlugin: NSObject, FlutterPlugin {
@@ -9,6 +10,34 @@ public class SwiftWalletPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    guard call.method.compare("presentAddPassViewController") == .orderedSame else {
+        return result(FlutterMethodNotImplemented);
+    }
+    
+    guard let args = call.arguments as? [String: Any],
+        let data = args["pkpass"] as? FlutterStandardTypedData,
+        data.elementCount > 0 else {
+        return result(FlutterError(code: "WITHOUT_PARAMETERS",
+                                   message: "Don't have 'pkpass' parameter",
+                                   details: "You need add 'pkpass' parameter"));
+    }
+    
+    do {
+        let newPass = try PKPass(data: data.data)
+        guard let addPassViewController = PKAddPassesViewController(pass: newPass) else {
+            return result(false);
+        }
+        
+        guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else {
+            return result(false);
+        }
+        rootVC.present(addPassViewController, animated: true) {
+            return result(true);
+        }
+    } catch {
+         return result(FlutterError(code: "WRONG_PARAMETER",
+                                    message: "Your 'pkpass' has wrong information",
+                                    details: "make sure pass your 'pkpass' in List<int> from Dart"));
+    }
   }
 }
